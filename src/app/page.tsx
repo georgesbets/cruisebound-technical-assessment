9a81e2375f099abb116087c0c5f9c3a45307db36
George Stock <georgesbulls@gmail.com>
2025-02-10 23:32:52 -0600

    testing vercel 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
       <div
         className={`flex flex-col w-[100%] min-w-[515px] p-6 pr-8 items-end`}
       >
-        test
+        test 2
         <CruiseFilterAndSortControls
           setSelectedOption={setCruiseSortSelectedOption}
           selectedOption={cruiseSortSelectedOption}
