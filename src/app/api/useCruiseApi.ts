8cb90336ba0c090c70df857d75f50de7a2a484f8
George Stock <george@continuoussoftware.com>
2025-02-10 23:03:46 -0600

    added cors to api get

--- src/app/api/useCruiseApi.ts
+++ src/app/api/useCruiseApi.ts
@@ -2,7 +2,12 @@ import { Cruise, RawCruise } from '@/app/definitions';
 
 const useCruiseApi = () => {
   const getCruises = async () => {
-    const response = await fetch('https://sandbox.cruisebound-qa.com/sailings');
+    const response = await fetch(
+      'https://sandbox.cruisebound-qa.com/sailings',
+      {
+        mode: 'cors',
+      }
+    );
 
     if (!response.ok) {
       throw new Error('failed to fetch ');
